6d335ec2e069b59a56c230e1a7a40600b646515d
eet172291 <eet172291@iitd.ac.in>
2018-09-27 16:49:06 +0530

    Updated

--- ps1.py
+++ ps1.py
@@ -2,61 +2,80 @@
 
 ####### write your code here ##########
 
-
+####### Code for problem 1 ######
 
 import numpy as np
 
+#function to find dimension of each cell
 def get_dim(cell, i, j):
+	#vectors that search in all 4 direction
 	x= [-1, 0, 1, 0]
 	y= [0, -1, 0, 1]
 	dim=-1
+	#getting shape of the matrix
 	n, m = np.array(cell).shape
+	#if the cell is dull cell then its dimension is -1
 	if cell[i][j]== 'D':
 		return dim
+	#else dimension is 1
 	else:
 		dim=1
 	num=1
+	#searching for largest cross around the cell
 	while True:
+		#checking for all 4 neighbour
 		for search in range(4):
 			newi= i+num*x[search]
 			newj= j+num*y[search]
+			#if any of the for neighbour is missing or any one is a dull cell then return with the previous dimension
 			if newi<0 or newi >=n or newj<0 or newj >=m or cell[newi][newj]== 'D':
 				return dim
+
+		#else dimension is increased by 4
 		dim= dim+4
 		num= num+1
 
+#getting input from user
 n= int(input("Enter n: "))
 m= int(input("Enter m: "))
+#checking for boundry condition
 if n<2 or n>105 or m<2 or m>105:
 	print("Wrong input")
 	exit()
-
+#getting the cells from user
 cell= [ [ None for y in range( m ) ] for x in range( n ) ]
 
 for i in range(n):
 	s= input()
 	for j in range(m):
 		cell[i][j]=s[j]
-	
+
+#checking if the cells are valid
 for i in range(n):
 	for j in range(m):
 		if cell[i][j]!= 'S' and cell[i][j]!= 'D':
 			print("Wrong input")
 			exit()
-
+#list to store dimension of all the cells
 dim= [ [ None for y in range( m ) ] for x in range( n ) ]
 
+
+#getting the dimension of all cell one by one
 for i in range(n):
 	for j in range(m):
 		dim[i][j]= get_dim(cell, i, j)
+
+#to get largest and second largest cross
 large= dim[0][0]
 sec_large= dim[0][0]
+#searching in the entire matrix, largest and second largest
 for i in range(n):
 	for j in range(m):
 		if dim[i][j]>= large:
 			sec_large= large			
 			large= dim[i][j]
 
+#displaying the result
 print(large, sec_large)		
 
 			
--- ps2.py
+++ ps2.py
@@ -2,11 +2,14 @@
 
 ####### write your code here ##########
 
+######Code for problem 2######
 
 import numpy as np
 
+#Getting input from user
 k= (input().split(" "))
 
+#checking for boundry conditions
 if len(k)!=3:
 	print("Wrong number of arguments")
 	exit()
@@ -16,11 +19,14 @@ for i in range(3):
 		print("Wrong input")
 		exit()
 
+#getting encrypted message from user
 en= input()
 if len(en) <1 or len(en)>150:
 	andprint("Wrong input")
 	exit()
 
+
+#list to store alphabet of all three groups and their index
 g1=[]
 g2=[]
 g3=[]
@@ -28,41 +34,55 @@ i1=[]
 i2=[]
 i3=[]
 
+#for entire message
 for i in range(len(en)):
+	#if it lies in 1st group the add to list one
 	if en[i]>='a' and en[i]<='i':
 		g1.append(en[i])
 		i1.append(i)
+	#if it lies in 2nd group the add to list two	
 	elif en[i]>='j' and en[i]<='r':
 		g2.append(en[i])
 		i2.append(i)
+	#if it lies in 3rd group the add to list three
 	elif (en[i]>='s' and en[i]<='z') or en[i]=='_':
 		g3.append(en[i])
 		i3.append(i)
+	#else the message contains some wrong character
 	else:
 		print("Wrong input")
 		exit()
 
 
 
-
+#rotating list g1 by k1
 newg1 = (g1[len(g1) - k[0]:len(g1)]  
-                 + g1[0:len(g1) - k[0]]) 
+                 + g1[0:len(g1) - k[0]])
+#rotating list g2 by k2 
 newg2 = (g2[len(g2) - k[1]:len(g2)]  
                  + g2[0:len(g2) - k[1]]) 
+#rotating list g3 by k3
 newg3 = (g3[len(g3) - k[2]:len(g3)]  
                  + g3[0:len(g3) - k[2]]) 
 
+#to store decrypted message
 de= [None]* len(en)
 
+#merging all the three list
+#merging list one
 for i in range(len(i1)):
-	de[i1[i]]= newg1[i]	
+	de[i1[i]]= newg1[i]
+#merging list two	
 for i in range(len(i2)):
-	de[i2[i]]= newg2[i]	
+	de[i2[i]]= newg2[i]
+#merging list three	
 for i in range(len(i3)):
 	de[i3[i]]= newg3[i]	
 
 
+#joining the list to form a string
 dec= "".join(de)
+#dsiplaying the decrypted message
 print(dec)
 
 
