c5bf1121d90daddab0f237d7597e3505b5a870e8
eet172291 <eet172291@iitd.ac.in>
2018-09-27 14:08:01 +0530

    Updated code

--- ps1.py
+++ ps1.py
@@ -1,3 +1,28 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+
+
+
+import numpy as np
+
+n= int(input("Enter n: "))
+m= int(input("Enter m: "))
+if n<2 or n>105 or m<2 or m>105:
+	print("Wrong input")
+	exit()
+
+cell= [ [ None for y in range( m ) ] for x in range( n ) ]
+
+for i in range(n):
+	s= input()
+	for j in range(m):
+		cell[i][j]=s[j]
+	
+for i in range(n):
+	for j in range(m):
+		if cell[i][j]!= 'S' and cell[i][j]!= 'D':
+			print("Wrong input")
+			exit()
+
+	
--- ps2.py
+++ ps2.py
@@ -1,3 +1,24 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+
+
+import numpy as np
+
+k= (input().split(" "))
+
+print(k)
+if len(k)!=3:
+	print("Wrong number of arguments")
+	exit()
+for i in range(3):
+	k[i]= int(k[i])
+	if k[i]<1 or k[i]>150:
+		print("Wrong input")
+		exit()
+
+encrypt= input()
+if len(encrypt) <1 or len(encrypt)>150:
+	print("Wrong input")
+	exit()
+
